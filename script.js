b8011f72af36364e8e8c11d4908ed74e7c5946ca
Weishler <ilcreatore321@gmail.com>
2022-02-03 14:59:46 -0400

    Draw the gameBoard

--- script.js
+++ script.js
@@ -2,8 +2,17 @@
 var gameBoard;
 var gameBoardContext;
 
+
+// Game Loop
 window.onload = function () {
   console.log("Hello World!");
   gameBoard = document.getElementById("gameBoard");
   gameBoardContext = gameBoard.getContext("2d");
+  setInterval(drawGame, 1000);
 };
+
+// Draw the game
+function drawGame() {
+  gameBoardContext.fillStyle = "black";
+  gameBoardContext.fillRect(0, 0, gameBoard.width, gameBoard.height);
+}
